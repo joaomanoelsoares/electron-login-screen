1abfd121a7a75aeaee183313b32d29dea31103d3
João Manoel <joaozinpaypal23@gmail.com>
2023-01-10 11:30:14 -0300

    All done

---
+++ src/js/appFunctions.js
@@ -0,0 +1,16 @@
+const { ipcRenderer } = require ('electron');
+const ipc = ipcRenderer;
+
+
+closeBtn.addEventListener('click', () => {
+    ipc.send('closeApp')
+})
+
+minimizeBtn.addEventListener('click', () => {
+    ipc.send('minimizeApp')
+})
+
+
+maxResBtn.addEventListener('click', () => {
+    ipc.send('maxResApp')
+})
